ff8e00446d39acca78a881fa65884fc3b2edc688
William Boas <william.boas@fretebras.com.br>
2025-09-24 18:32:57 -0300

    fix: monoglog

--- src/Providers/LoggerServiceProvider.php
+++ src/Providers/LoggerServiceProvider.php
@@ -3,6 +3,9 @@
 namespace LogFormatter\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use LogFormatter\Contracts\FormatterInterface;
 use LogFormatter\Contracts\LoggerInterface;
 use LogFormatter\Formatters\JsonFormatter;
@@ -20,6 +23,23 @@ class LoggerServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // 1️⃣ Registrar middleware
         $this->app['router']->pushMiddlewareToGroup('api', LogRequestResponse::class);
+
+        // 2️⃣ Substituir o log default do Laravel
+        Log::extend('logformatter', function ($app, $config) {
+            $monolog = new Logger('logformatter');
+            $handler = new StreamHandler(storage_path('logs/laravel.log'));
+
+            // Pega a implementação do formatter da SDK
+            $formatter = $app->make(FormatterInterface::class);
+            $handler->setFormatter($formatter);
+
+            $monolog->pushHandler($handler);
+            return $monolog;
+        });
+
+        // 3️⃣ Define este canal como default em tempo de execução
+        $this->app['config']->set('logging.default', 'logformatter');
     }
 }
