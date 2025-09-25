ce7de4410976fa60be8e3e07cb5f0486fd3a7722
William Boas <william.boas@fretebras.com.br>
2025-09-25 14:50:42 -0300

    fix: ajustes

--- src/Formatters/JsonFormatter.php
+++ src/Formatters/JsonFormatter.php
@@ -7,19 +7,19 @@ use Monolog\LogRecord;
 
 class JsonFormatter implements FormatterInterface
 {
-    public function format(LogRecord $record)
+    public function format(LogRecord $record): string
     {
         return json_encode([
-                'application'   => env('APP_NAME', 'laravel'),
-                'environment'   => env('APP_ENV', 'local'),
-                'level'         => $record['level_name'],
-                'message'       => $record['message'],
-                'context'       => $record['context'],
-                'datetime'      => $record['datetime']->format('c'),
+                'application' => env('APP_NAME', 'laravel'),
+                'environment' => env('APP_ENV', 'local'),
+                'level'       => $record->level->getName(),
+                'message'     => $record->message,
+                'context'     => $record->context,
+                'datetime'    => $record->datetime->format('c'),
             ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . PHP_EOL;
     }
 
-    public function formatBatch(array $records)
+    public function formatBatch(array $records): array
     {
         return array_map([$this, 'format'], $records);
     }
